db93601d909c036e5747f8dc05ff8c5e6966a110
nicolas gronskis <nicogrons@gmail.com>
2022-01-30 09:47:30 -0300

    solucionada la codificacion al devolver json

--- vistas/ajax/index.php
+++ vistas/ajax/index.php
@@ -1,4 +1,17 @@
 <?php
 
+    function utf8ize($mixed) {
+        if (is_array($mixed)) {
+            foreach ($mixed as $key => $value) {
+                $mixed[$key] = utf8ize($value);
+            }
+        } else if (is_string ($mixed)) {
+            return utf8_encode($mixed);
+        }
+        return $mixed;
+    }
+
     header('Content-type: application/json');
-    echo json_encode( $data["registros"] );
+
+    echo json_encode( utf8ize($data["registros"]) );
+             
